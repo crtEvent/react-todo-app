2c7c1f29cb296e4e5624f2a705ef599707ad7435
crtEvent <crtevn@gmail.com>
2023-05-31 00:06:18 +0900

    LoginComponent username input 값 수정 가능하도록 변경

--- src/components/todo/TodoApp.jsx
+++ src/components/todo/TodoApp.jsx
@@ -1,3 +1,4 @@
+import { useState } from 'react'
 import './TodoApp.css'
 
 export default function TotoApp() {
@@ -10,12 +11,19 @@ export default function TotoApp() {
 }
 
 function LoginComponent() {
+
+    const [username, setUserName] = useState('admin')
+
+    function handleUserNameChange(event) {
+        setUserName(event.target.value)
+    }
+
     return (
         <div className="Login">
             <div className="LoginForm">
                 <div>
                     <label>User Name</label>
-                    <input type="text" name="username" />
+                    <input type="text" name="username" value={username} onChange={handleUserNameChange} />
                 </div>
                 <div>
                     <label>Password</label>
