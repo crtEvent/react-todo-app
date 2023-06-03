748c2869ace2ff9892df890c0e30799a50b3b3e4
crtEvent <crtevn@gmail.com>
2023-06-03 14:42:03 +0900

    로그인시 welcomeCOmponent에 username 파라미터 넘기기 추가

--- src/components/todo/TodoApp.jsx
+++ src/components/todo/TodoApp.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react'
-import { BrowserRouter, Route, Routes, useNavigate } from 'react-router-dom'
+import { BrowserRouter, Route, Routes, useNavigate, useParams } from 'react-router-dom'
 import './TodoApp.css'
 
 export default function TotoApp() {
@@ -10,7 +10,7 @@ export default function TotoApp() {
                 <Routes>
                     <Route path='/' element={<LoginComponent />}></Route>
                     <Route path='/login' element={<LoginComponent />}></Route>
-                    <Route path='/welcome' element={<WelcomeComponent />}></Route>
+                    <Route path='/welcome/:username' element={<WelcomeComponent />}></Route>
                     <Route path='/*' element={<ErrorComponent />}></Route>
                 </Routes>
             </BrowserRouter>
@@ -40,7 +40,7 @@ function LoginComponent() {
         if(username === 'admin' && password === '1234') {
             setShowSuccessMessage(true)
             setShowErrorMessage(false)
-            navigate('/welcome')
+            navigate(`/welcome/${username}`)
         } else {
             setShowSuccessMessage(false)
             setShowErrorMessage(true)
@@ -70,9 +70,13 @@ function LoginComponent() {
 }
 
 function WelcomeComponent() {
+
+    const { username } = useParams()
+    console.log(username)
+
     return (
         <div className='WelcomeComponent'>
-            <h1>Welcome!</h1>
+            <h1>Welcome! { username }</h1>
             TODO
         </div>
     )
