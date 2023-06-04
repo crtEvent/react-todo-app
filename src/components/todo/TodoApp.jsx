945f466882f7c0a10779b6ce377b2338650969c0
crtEvent <crtevn@gmail.com>
2023-06-04 11:29:39 +0900

    인증 라우터로 React 라우터 보호하기

--- src/components/todo/TodoApp.jsx
+++ src/components/todo/TodoApp.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom'
 import LoginComponent from './LoginComponent.jsx'
 import LogoutComponent from './LogoutComponent.jsx'
 import HeaderComponent from './HeaderComponent.jsx'
@@ -6,9 +6,18 @@ import FooterComponent from './FooterComponent.jsx'
 import ListTodosComponent from './ListTodosComponent.jsx'
 import ErrorComponent from './ErrorComponent.jsx'
 import WelcomeComponent from './WelcomeComponent.jsx'
-import AuthProvider from './security/AuthContext'
+import AuthProvider, { useAuth } from './security/AuthContext'
 
 import './TodoApp.css'
+
+function AuthenticatedRoute({children}) {
+    const authContenxt = useAuth()
+    
+    if(authContenxt.isAuthenticated)
+        return children
+
+    return <Navigate to="/" />
+}
 
 export default function TotoApp() {
     return (
@@ -19,9 +28,9 @@ export default function TotoApp() {
                     <Routes>
                         <Route path='/' element={<LoginComponent />} />
                         <Route path='/login' element={<LoginComponent />} />
-                        <Route path='/welcome/:username' element={<WelcomeComponent />} />
-                        <Route path='/todos' element={<ListTodosComponent />} />
-                        <Route path='/logout' element={<LogoutComponent />} />
+                        <Route path='/welcome/:username' element={<AuthenticatedRoute><WelcomeComponent /></AuthenticatedRoute>} />
+                        <Route path='/todos' element={<AuthenticatedRoute><ListTodosComponent /></AuthenticatedRoute>} />
+                        <Route path='/logout' element={<AuthenticatedRoute><LogoutComponent /></AuthenticatedRoute>} />
 
                         <Route path='/*' element={<ErrorComponent />} />
                     </Routes>
